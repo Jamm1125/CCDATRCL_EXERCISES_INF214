934b1bdc2238a384e7544dbf0c938610123e1e04
Jamm1125 <111845525+Jamm1125@users.noreply.github.com>
2022-08-26 16:29:37 +0800

    Update Weapon.java

--- exercise_1/Weapon.java
+++ exercise_1/Weapon.java
@@ -1,1 +1,20 @@
+public class Weapon {
+    String name;
+    int damage;
+    int weight;
+    String rarity;
+    String weapondescription;
+
+
+    public void weaponDescription() {
+        System.out.println( name + " \nBurns the target");
+    }
+
+    public void weaponDescription2() {
+            System.out.println( name + " \nSpread on the ground and it creates wall of fire");
+    }
+    public void weaponDescription3() {
+        System.out.println( name + " \nIt Freezes and shocks the target");
+    }
+}
 
